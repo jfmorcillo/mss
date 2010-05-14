aeacb083e889c5a390530b344f0496c15f5f2ce6
jpbraun <jpbraun@d2e75950-e32a-47fb-b4e0-77284bb43e1d>
2010-05-14 14:11:03 +0000

    * add ignore properties on all reps * add translations files * new images
    git-svn-id: svn+ssh://svn.mandriva.com/svn/private/corporate/soft/mmc-wizard/branches/wizard-2@2093 d2e75950-e32a-47fb-b4e0-77284bb43e1d

--- agent/modules/mds-quota/setup-quota.sh
+++ agent/modules/mds-quota/setup-quota.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+. ../functions.sh
+
+userquota_tpl="templates/userquota.ini.tpl"
+
 filesystems=$1
 
-echo $1
+for fs in $filesystems; do
+    tmp=$fs";"
+done
+
+# install template
+backup /etc/mmc/plugins/userquota.ini
+cat $userquota_tpl > /etc/mmc/plugins/userquota.ini
+sed -i "s/\@FILESYSTEMS\@/$tmp/" /etc/mmc/plugins/userquota.ini
+
+# add user quota option
+# backup /etc/fstab
 
 sleep 1
 exit 0
