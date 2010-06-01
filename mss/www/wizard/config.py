2a01a6c1ef772279007e697365ae71bd1e0ce28e
jpbraun <jpbraun@d2e75950-e32a-47fb-b4e0-77284bb43e1d>
2010-06-01 12:20:42 +0000

    * change path
    git-svn-id: svn+ssh://svn.mandriva.com/svn/private/corporate/soft/mmc-wizard/branches/wizard-2@2151 d2e75950-e32a-47fb-b4e0-77284bb43e1d

--- mss/www/wizard/config.py
+++ mss/www/wizard/config.py
@@ -10,8 +10,10 @@ class ConfigManager:
         self.load_configs()
 
     def load_configs(self):
-        for config in glob.glob(os.path.join(os.path.dirname(__file__), "modules", "*.py")):
+        for config in glob.glob(os.path.join(os.path.dirname(__file__), 
+            "..", "layout", "*", "config.py")):
             name = config.split("/")[-1].rstrip('.py')
+            print name
             module = imp.load_source(name, config)
             if hasattr(module, "config"):
                 self.SECTIONS += module.config.SECTIONS
