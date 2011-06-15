4c25b4c144a7398abeeeba497c5a53d7aafe4c71
Jean-Philippe Braun <jpbraun@mandriva.com>
2011-06-15 11:03:26 +0200

    escape password for sed

--- mss/agent/modules/functions.sh
+++ mss/agent/modules/functions.sh
@@ -157,3 +157,13 @@ function restart_service() {
         exit 1
     fi
 }
+
+# escapestr_sed()
+# read a stream from stdin and escape characters in text that could be interpreted as
+# special characters by sed
+escape_sed() {
+ echo $1 | \
+ sed \
+  -e 's/\//\\\//g' \
+  -e 's/\&/\\\&/g'
+}
