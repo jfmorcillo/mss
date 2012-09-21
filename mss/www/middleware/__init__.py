b5e1192ea009f4f3203fe9f1872224053edcabd3
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-09-21 10:45:43 +0200

    Fix middleware JS

--- mss/www/middleware/__init__.py
+++ mss/www/middleware/__init__.py
@@ -9,14 +9,27 @@ class CatchAjaxException(object):
         if request.is_ajax():
             url = request.META['REQUEST_URI']
             error = """
-            var message = new Element('p', {'class': 'error'});
-            message.update("Error while calling %s");
-            var log = new Element('div', {'class': 'log'});
-            log.update("%s");
-            $('popup').appendChild(message);
-            $('popup').appendChild(log);
-            $("popup").addClassName("poptb");
-            $('popup').show();
+	    var modal = $('<div>', { class: "modal hide fade" });
+	    var modal_header = $('<div>', { class: "modal-header" });
+	    var modal_title = $('<h3>');
+	    var modal_body = $('<div>', { class: "modal-body" });
+	    var modal_message = $('<pre>');
+	    var modal_footer = $('<div>', { class: "modal-footer" });
+	    var modal_close = $('<a>', { href: "#" });
+
+	    modal_title.html("Error while calling %s");
+	    modal_message.html("%s");
+	    modal_close.html("{% trans "Close" %}")
+
+	    modal_body.append(modal_message);
+	    modal_header.append(modal_title);
+	    modal_close.data("dismiss", "modal");
+	    modal_footer.append(modal_close);
+	    modal.append(modal_header);
+	    modal.append(modal_body);
+	    modal.append(modal_footer);
+
+	    $(modal).modal('show');
             """ % (url, escape(traceback.format_exc()).replace('\n', '\\n'))
 
             return HttpResponse(error, mimetype="application/javascript")
