5190513d01f04fdccbc97aa6923f2c751a30665c
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-10-28 12:41:49 +0100

    Fix shorewall rule

--- mss/agent/modules/network/setup-network.sh
+++ mss/agent/modules/network/setup-network.sh
@@ -39,7 +39,7 @@ function begin_shorewall_conf() {
 function build_shorewall_conf() {
     echo $2 $1 detect >> $shorewall_interfaces
     echo $2 ipv4 >> $shorewall_zones
-    echo ACCEPT/SSH     $2  fw >> $shorewall_rules
+    echo SSH/ACCEPT     $2  fw >> $shorewall_rules
     echo ACCEPT         $2  fw  tcp     8000 >> $shorewall_rules
 }
 
