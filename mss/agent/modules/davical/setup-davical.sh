21f7ccee8ccd6755f0137404cda93261431e9276
Jean-François Morcillo <jfmorcillo@mandriva.com>
2012-11-15 14:24:00 +0100

    cleanup

--- mss/agent/modules/davical/setup-davical.sh
+++ mss/agent/modules/davical/setup-davical.sh
@@ -33,14 +33,14 @@ for (( i=20 ; i ; i-=1 )); do sleep 1; echo -n '.'; done
 ###Create Davical DB
 #TODO: check that DB don't already exists
 su postgres -c /usr/share/davical/dba/create-database.sh 2>&1
-#when this script ends it prints the admin password it should be printed to the user of MSS
+#when this script ends it prints the admin password it must be printed to the user of MSS
 #get the generated password from the DB
 DAVICAL_ADMIN_PASS=`su postgres -c "psql davical -c 'select username, password from usr;'" | sed -n '/admin/s/^ *admin.*\*\*\(.*\)$/\1/p'`
 
 if [ ! $DAVICAL_ADMIN_PASS ];
 then
-    echo "Something goes wrong with the database setup"
-    echo "Check the logs"
+    echo "1Something goes wrong with the database setup"
+    echo "1Check the logs"
     exit 1
 fi
 
@@ -59,12 +59,10 @@ sed -i "s/\@SUFFIX\@/$MDSSUFFIX/" $DAVICAL_CONF
 
 restart_service $SERVICE
 
+###Open the firewall port
+#No need to open a particular port as Davical listen on the standard http port that should be opened
+
 echo "8The calendar and addressbook server is configured."
 echo "7The temporary admin password is '$DAVICAL_ADMIN_PASS' (without the ' ')"
 echo "7Remember to change it using the manadement interface http://@HOSTNAME@/davical/admin.php"
 
-
-#echo "8The instant messaging service is configured."
-#echo "8You can simply add users through the management interface at# http://@HOSTNAME@/mmc/. #The user's mail will be used as his Jabber ID."
-#echo "7Configure clients with JID like# @B@uid@$domain@B@ #and use# @B@$hostname.$domain@B@ #as the server."
-#echo "7If# @B@$hostname.$domain@B@ #is registered as a Jabber server in the DNS zone# @B@$domain@B@ #there is no need to configure the server address in clients."
