93d22b4e8248f0d876d3563ef0eab2a74c720140
Laurent Fazio <lfazio@mandriva.com>
2013-03-25 15:39:03 +0100

    module: sshlpk: remove unneed step and copy LDAP schema in MSS module
    Signed-off-by: Laurent Fazio <lfazio@mandriva.com>

--- mss/agent/modules/sshlpk/setup-sshlpk.sh
+++ mss/agent/modules/sshlpk/setup-sshlpk.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Copyright Mandriva 2013 all rights reserved
 
+# Include toolbox
 . '../functions.sh'
 
 check_mmc_configured
 
-# Create configuration file from template
-cat tempaltes/sshlpk.ini.tpl > /etc/mmc/plugins/sshlpk.ini
-
 # Import LDAP schema
-add_schema /usr/share/doc/python-mmc-base/contrib/ldap/sshlpk.schema
+add_schema ldap/openssh-lpk.schema
 
 # Restart mmc agent
 restart_service mmc-agent /var/log/mmc/mmc-agent.log
+
 
 exit 0
--- mss/agent/modules/sshlpk/templates/sshlpk.ini.tpl
+++
@@ -1,8 +0,0 @@
-[main]
-disable = no
-
-[hooks]
-### The script will be run as root user, with as only argument the full LDIF of the LDAP user
-###
-###
-#updatesshkeys = /path/to/script/run/on/sshkey/update
