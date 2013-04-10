dccdfdd1e09945d24410e26da3decb3f773f3d7e
Jean-Philippe Braun <eon@patapon.info>
2013-04-10 13:52:22 +0200

    packaging: include branch name in the tarball

--- build_sdist.sh
+++ build_sdist.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+git pull
 git clean -fdx
 sh build_mo.sh
 gitversion=-`git describe --always --long`
-python setup.py setopt -o tag_build -s $gitversion -c egg_info
+gitbranch=`git rev-parse --abbrev-ref HEAD`
+python setup.py setopt -o tag_build -s $gitversion-$gitbranch -c egg_info
 python setup.py setopt -o tag_date -s 1 -c egg_info
 python setup.py sdist
