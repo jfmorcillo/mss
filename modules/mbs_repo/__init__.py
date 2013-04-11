e558c9c08fcf35301378c31d66507d1052a54ef8
Jean-Philippe Braun <jpbraun@mandriva.com>
2013-04-11 22:22:32 +0200

    Check if the repo is already configured

--- modules/mbs_repo/__init__.py
+++ modules/mbs_repo/__init__.py
@@ -1,3 +1,9 @@
 
+from mss.agent.lib.utils import grep
+
 def get_config_info():
     return ('setup-repo.sh', [])
+
+def check_configured():
+    return grep('mirror.mandriva.com/mbs1/x86_64', '/etc/urpmi/urpmi.cfg')
+
