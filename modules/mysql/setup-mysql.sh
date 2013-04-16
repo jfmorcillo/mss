b9b569130a76f350e5d5db81c653c5434ee70b12
Laurent Fazio <lfazio@mandriva.com>
2013-04-16 16:50:35 +0200

    module: mysql: Keep .my.cnf file in root home
    Signed-off-by: Laurent Fazio <lfazio@mandriva.com>

--- modules/mysql/setup-mysql.sh
+++ modules/mysql/setup-mysql.sh
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-config=".my.cnf.$$"
+config="/root/.my.cnf"
 command=".mysql.$$"
 
 trap "interrupt" 2
@@ -172,7 +172,7 @@ interrupt() {
 
 cleanup() {
     echo "Cleaning up..."
-    rm -f $config $command
+    rm -f $command
 }
 
 
