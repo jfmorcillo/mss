ed63110080b9bed16cf002f07b2baea607b6cbe1
Jean-Philippe Braun <jpbraun@mandriva.com>
2013-06-08 08:49:20 +0200

    module davical: configuration summary

--- modules/davical/setup-davical.sh
+++ modules/davical/setup-davical.sh
@@ -85,7 +85,7 @@ sed -i "s/\@FQDN\@/$FQDN/" $DAVICAL_CRON
 
 info_b $"The calendar and addressbook service is configured."
 
-info_b $"Use https://@HOSTNAME@/davical/caldav.php/[user_email]/calendar/ to add calendar into caldav clients."
-info_b $"You can add share calendars at https://@HOSTNAME@/mmc/"
+info $"- each user can access its personnal calendar at https://@HOSTNAME@/davical/caldav.php/[user_email]/calendar/ using a calendar client (Thunderbird with Lightning, iCal...)"
+info $"- you can add shared calendars at https://@HOSTNAME@/mmc/"
 
 exit 0
