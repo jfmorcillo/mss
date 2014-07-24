0c118f52a3beabd856b98014ce47cab569563ce6
Jesús García Sáez <jgarcia@zentyal.com>
2014-07-24 11:17:28 +0200

    validate samba admin password

--- modules/mds_samba4/__init__.py
+++ modules/mds_samba4/__init__.py
@@ -13,3 +13,14 @@ def valid_domain(string):
         return _("Incorrect SAMBA domain name.", "mds_samba4")
     else:
         return None
+
+def valid_password(passwd):
+    if len(passwd) < 8:
+        return _("Password must be at least 8 characters long", "mds_samba4")
+    if not re.search("[0-9]", passwd):
+        return _("Password must have at least one number", "mds_samba4")
+    if not re.search("[A-Z]", passwd):
+        return _("Password must have at least one capital letter", "mds_samba4")
+    if not re.search("[a-z]", passwd):
+        return _("Password must have at least one lowercase letter", "mds_samba4")
+    return None
