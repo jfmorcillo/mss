72d9d6ae90a81c11faa7f566e9f63622fcde975b
Jesús García Sáez <jgarcia@zentyal.com>
2014-07-24 11:11:54 +0200

    remove zentyal repo as module

--- modules/repo_zentyal_jgarcia/__init__.py
+++
@@ -1,7 +0,0 @@
-from mss.agent.lib.utils import grep
-
-def get_config_info():
-    return ('setup-repo.sh', [])
-
-def check_configured():
-    return grep('zentyal_jgarcia', '/etc/urpmi/urpmi.cfg')
--- modules/repo_zentyal_jgarcia/setup-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-urpmi.addmedia zentyal_jgarcia http://people.zentyal.org/~jgarcia/rpms && \
-echo "Done"
-
-exit $?
