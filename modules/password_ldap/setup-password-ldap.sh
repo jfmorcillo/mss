9a11da9223d0f1ecc22cab805e5bedda8ac93306
Jean-Philippe Braun <jpbraun@mandriva.com>
2014-08-26 12:29:35 +0200

    Mark more strings for translation

--- modules/password_ldap/setup-password-ldap.sh
+++ modules/password_ldap/setup-password-ldap.sh
@@ -10,8 +10,8 @@ new_password=$2
 
 # changing the LDAP password
 rootDN="uid=LDAP Admin, ou=System Accounts, $MDSSUFFIX"
-echo Changing password for $rootDN...
-ldappasswd -x -D "$rootDN" -h $MDSSERVER -w $old_password -s $new_password && info_b "Password changed." || (error "Failed to change the password."; exit 1)
-sed -i "s!^password =.*!password = $new_password!" /etc/mmc/plugins/base.ini || (error "Failed to change the password."; ldappasswd -x -D "$rootDN" -h $MDSSERVER -w $new_password -s $old_password; exit 1)
+echo "Changing password for $rootDN..."
+ldappasswd -x -D "$rootDN" -h $MDSSERVER -w $old_password -s $new_password && info_b $"Password changed." || (error $"Failed to change the password."; exit 1)
+sed -i "s!^password =.*!password = $new_password!" /etc/mmc/plugins/base.ini || (error $"Failed to change the password."; ldappasswd -x -D "$rootDN" -h $MDSSERVER -w $new_password -s $old_password; exit 1)
 
 restart_service mmc-agent
--- modules/password_root/setup-password-root.sh
+++ modules/password_root/setup-password-root.sh
@@ -10,8 +10,8 @@ new_password=`escape_sed $2`
 
 result=`python check_root_password.py $old_password`
 if [ $result -eq 0 ]; then
-    echo root:$new_password | /usr/sbin/chpasswd && (info_b "The password has been changed."; exit 0) || (error "Failed to change the password."; exit 1)
+    echo root:$new_password | /usr/sbin/chpasswd && (info_b $"The password has been changed."; exit 0) || (error $"Failed to change the password."; exit 1)
 else
-    error "Current password is not correct."
+    error $"Current password is not correct."
     exit 1
 fi
