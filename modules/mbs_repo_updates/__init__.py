054925aed3aff966fb52b6875bf230990021ced8
Jean-Philippe Braun <eon@patapon.info>
2014-10-06 15:50:33 +0200

    Fix repos for MBS2

--- modules/mbs_repo_updates/__init__.py
+++ modules/mbs_repo_updates/__init__.py
@@ -1,5 +1,4 @@
 from mss.agent.lib.utils import grep
 
 def check_configured():
-    return grep('mirror.mandriva.com/mbs1_updates/x86_64', '/etc/urpmi/urpmi.cfg')
-
+    return grep('mirror.mandriva.com/factory/x86_64', '/etc/urpmi/urpmi.cfg')
