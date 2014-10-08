d5bc6c694624f71403f3539ad8893bf9744d6df4
Jean-Philippe Braun <eon@patapon.info>
2014-10-08 17:01:02 +0200

    Remove debug mode

--- mss/www/settings.py
+++ mss/www/settings.py
@@ -22,7 +22,6 @@
 import os
 import locale
 import sys
-import glob
 import logging
 import logging.handlers
 
@@ -32,7 +31,7 @@ ADMINS = (('root', 'root@localhost'),)
 MANAGERS = ADMINS
 PROJECT_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 EMAIL_SUBJECT_PREFIX = "[MSS]"
