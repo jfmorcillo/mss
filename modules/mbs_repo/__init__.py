941093db568ad89648e543291f00ea9f1c734fed
Jean-Philippe Braun <eon@patapon.info>
2014-12-09 14:20:05 +0100

    repos: factory -> mbs2

--- modules/mbs_repo/__init__.py
+++ modules/mbs_repo/__init__.py
@@ -2,4 +2,4 @@ from mss.agent.lib.utils import grep
 
 
 def check_configured():
-    return grep('mirror.mandriva.com/factory/x86_64', '/etc/urpmi/urpmi.cfg')
+    return grep('mirror.mandriva.com/mbs2/x86_64', '/etc/urpmi/urpmi.cfg')
