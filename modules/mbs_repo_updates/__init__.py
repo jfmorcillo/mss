2c328f3762d93e7ea97ed501437953f71314d1cd
Jean-Philippe Braun <eon@patapon.info>
2015-01-08 15:00:41 +0100

    mbs_repo_updates: fix repo url

--- modules/mbs_repo_updates/__init__.py
+++ modules/mbs_repo_updates/__init__.py
@@ -1,4 +1,5 @@
 from mss.agent.lib.utils import grep
 
+
 def check_configured():
-    return grep('mirror.mandriva.com/mbs2/x86_64', '/etc/urpmi/urpmi.cfg')
+    return grep('mirror.mandriva.com/mbs2_updates/x86_64', '/etc/urpmi/urpmi.cfg')
