6fca9a6f8e37fb9910cbc4150d3061fc8607d2f5
Jean-Philippe Braun <jpbraun@mandriva.com>
2015-03-03 14:53:03 +0100

    network: handle none type, support eno interface
    If the interface type is none, it will not be configured in shorewall.
    
    Also, support en* interface (enp, eno, etc...)

--- modules/network/__init__.py
+++ modules/network/__init__.py
@@ -20,6 +20,7 @@
 # MA 02110-1301, USA.
 
 import os
+import re
 import netifaces
 
 from mss.agent.lib.utils import grep, get_config_option, MANAGED_INTERFACE_NAMES
@@ -52,6 +53,7 @@ def get_interfaces_config(config):
     CONFIG_DIR = "/etc/sysconfig/network-scripts"
     for interface in netifaces.interfaces():
         if interface.startswith(MANAGED_INTERFACE_NAMES):
+            interface_id = re.sub(r'[a-z]', '', interface)
             if_file = os.path.join(CONFIG_DIR, "ifcfg-%s" % interface)
             if_detail = netifaces.ifaddresses(interface)
             configured = os.path.exists(if_file) and netifaces.AF_INET in if_detail
@@ -71,6 +73,8 @@ def get_interfaces_config(config):
             # need a type even if not configured
             if not configured:
                 type = "none"
+            else:
+                type = "lan" + interface_id
             if configured:
                 if grep("BOOTPROTO=dhcp", if_file):
                     method = "dhcp"
@@ -96,15 +100,15 @@ def get_interfaces_config(config):
                            'type': 'text', 'hidden': 'yes', 'default': interface})
             config.append({'slug': 'network',
                            'name': interface + '_type',
-                           'show_if_unconfigured': 'yes',
                            'require': 'yes',
                            'default': type,
                            'label': _('Interface type', 'network'),
                            'help': _('Choose "External" if the interface is connected to the Internet. If the interface is connected to an internal network, choose "Internal"', 'network'),
                            'type': 'options',
                            'options': [
-                               {'name': _('Internal network', 'network'), 'value': 'lan' + interface[-1]},
-                               {'name': _('External network', 'network'), 'value': 'wan' + interface[-1]}
+                               {'name': _('No configuration', 'network'), 'value': 'none'},
+                               {'name': _('Internal network', 'network'), 'value': 'lan' + interface_id},
+                               {'name': _('External network', 'network'), 'value': 'wan' + interface_id}
                            ]
                            })
             config.append({'slug': 'network',
